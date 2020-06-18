6fb01cde6f0d4710159e227dd4707b0c393685d1
Rakankh <64202526+Rakankh@users.noreply.github.com>
2020-06-18 17:29:33 +0300

    18-6/2

--- tictoe.js
+++ tictoe.js
@@ -13,14 +13,23 @@ var c1 = document.getElementById("c1")
 var c2 = document.getElementById("c2")
 var c3 = document.getElementById("c3")
 
-a1.addEventListener("mouseover", myFunction)
+a1.addEventListener("click", myFunction)
+// can we make all of the table elements hidden button by css : display : none 
+//then assign the X/O inside it by event listner !?
+
+// we should add a function just like Nqueens based on arrays 
+// make 3 arrays / each one includes 3 indexes 
+// if ( array1[0] && array2[0] && array3[0]=== x ) alert you win 
+// if ( array1[1] && array2[1] && array3[1]=== x ) alert you win
+// if ( array1[2] && array2[2] && array3[2]=== x ) alert you win
 
 
+// if ( array1[0] && array2[1] && array3[2]=== x ) alert you win
+// if ( array1[] && array2[] && array3[]=== x ) alert you win
+// if ( array1[1] && array2[1] && array3[1]=== x ) alert you win
 
 
-
-
-
+// ma3gool ??? this way we will need so many conditionns 
 
 
 function myFunction (){
